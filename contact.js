519fe3e16480c0942e0bdf2b84ee0e1ee0c5db31
siddhant-sapkota10 <107187347+siddhant-sapkota10@users.noreply.github.com>
2022-08-20 16:48:18 +1000

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const btnOpenModal = document.getElementById("contact-btn")
+const btnCloseModal = document.querySelector(".close-modal")
+const overlay = document.querySelector(".overlay")
+
+btnOpenModal.addEventListener("click", function(){
+    openModal()
+})
+
+btnCloseModal.addEventListener("click", function(){
+    closeModal()
+})
+
+const openModal = function(){
+modal.classList.remove("hidden")
+overlay.classList.remove("hidden")
+}
+
+const closeModal = function(){
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");  
+}
+
+document.addEventListener("keydown", function (e){
+    if(e.key === "Escape" && !modal.classList.contains("hidden")){
+        closeModal()
+    }
+})
